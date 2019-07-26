095ef7e03b03727086d4ebce58f100e48d64762e
ADIL <globalmail91@gmail.com>
2019-07-27 00:20:14 +0600

    Добавьте файлы проекта.

---
+++ MvcCars/Data/MvcCarsContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MvcCars.Models
+{
+    public class MvcCarsContext : DbContext
+    {
+        public MvcCarsContext (DbContextOptions<MvcCarsContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MvcCars.Models.Car> Car { get; set; }
+    }
+}
